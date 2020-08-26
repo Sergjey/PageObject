7164f15e30e416f66821b9918f186d5ea6a6a0a9
Sergjey <Strongestiq@gmail.com>
2020-08-26 16:06:50 +0300

    Page directory created, base_page.py created, main_page.py created

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,10 @@
-link = "http://selenium1py.pythonanywhere.com/"
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link) #инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
+    page.open()
+    page.go_to_login_page() #выполняем метод страницы - открываем страницу логина
+    #browser.get(link)
+    #login_link = browser.find_element_by_css_selector("#login_link")
+    #login_link.click()
