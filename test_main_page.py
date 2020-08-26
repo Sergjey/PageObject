160b00412c7c0cad50a6850573ad523d5e007256
Sergjey <Strongestiq@gmail.com>
2020-08-26 22:30:07 +0300

    добавлены переходы на страницы

--- test_main_page.py
+++ test_main_page.py
@@ -7,8 +7,11 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link) #инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
     page.open()
-    #time.sleep(5)
+    time.sleep(5)
     page.go_to_login_page() #выполняем метод страницы - открываем страницу логина
+    login_page = page.go_to_login_page()#осуществляем переход на страницу с логином
+    login_page.should_be_login_page()
+    time.sleep(5)
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
