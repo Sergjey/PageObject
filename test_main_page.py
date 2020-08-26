1db8365f60cade4dc3642982e670ffd273458179
Sergjey <Strongestiq@gmail.com>
2020-08-26 16:20:43 +0300

    comments deleted

--- test_main_page.py
+++ test_main_page.py
@@ -5,6 +5,3 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link) #инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
     page.open()
     page.go_to_login_page() #выполняем метод страницы - открываем страницу логина
-    #browser.get(link)
-    #login_link = browser.find_element_by_css_selector("#login_link")
-    #login_link.click()
