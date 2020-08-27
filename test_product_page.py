932898fcb656001265e5aa82b2eb51d87db41777
Sergjey <Strongestiq@gmail.com>
2020-08-27 23:12:01 +0300

    add math formula to take a code

--- test_product_page.py
+++ test_product_page.py
@@ -9,4 +9,5 @@ def test_guest_can_add_product_to_basket(browser):
     #time.sleep(5)
     page.should_be_item()
     page.add_item_in_basket()
-    time.sleep(5)
+    page.solve_quiz_and_get_code()
+    time.sleep(3)
